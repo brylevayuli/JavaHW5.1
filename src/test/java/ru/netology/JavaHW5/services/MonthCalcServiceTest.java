347439d8cfbf693494dec39fa05cb40e48a03bc2
Julia <yinwhite88@gmail.com>
2023-09-12 00:33:29 +0300

    second commit

--- src/test/java/ru/netology/JavaHW5/services/MonthCalcServiceTest.java
+++ src/test/java/ru/netology/JavaHW5/services/MonthCalcServiceTest.java
@@ -2,38 +2,44 @@ package ru.netology.JavaHW5.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.JavaHW5.services.MonthCalcService;
 
 
 public class MonthCalcServiceTest {
 
-    @Test
-    public void testForThreeMonthOfRest() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000",
+//    })
+
+    @CsvFileSource(files="src/test/resources/month.csv")
+    public void testForThreeMonthOfRest(int expected, int income, int expenses, int threshold) {
         MonthCalcService service = new MonthCalcService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
 
-        int expected = 3;
+//        int expected = 3;
         int actual = service.calcMonth(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    public void testForTwoMonthOfRest() {
-        MonthCalcService service = new MonthCalcService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
-        int actual = service.calcMonth(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
+//    @Test
+//    public void testForTwoMonthOfRest() {
+//        MonthCalcService service = new MonthCalcService();
+//
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//
+//        int expected = 2;
+//        int actual = service.calcMonth(income, expenses, threshold);
+//
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
 }
